ea63a022037722c844baa0f377d656a037313864
LailaMDuarte <124358830+LailaMDuarte@users.noreply.github.com>
2023-02-07 11:29:18 -0300

    aula 07

---
+++ aula07/script/variaveis.js
@@ -0,0 +1,22 @@
+// tem duas formas de declarar uma variavel
+// var e let
+
+//let mensagem;
+//mensagem = "Olá mundo";
+
+//alert(mensagem); //alerta na pagina principal no navegador
+
+//console.log(Reflect.reservedWords);
+
+/*let hello = 'Olá turma da manhã'
+let mensagem; 
+
+mensagem = hello;
+
+alert(hello);
+alert(mensagem);*/
+
+
+const carro = ["fiat", "golf", "bmw", "ford"]
+
+alert(carro); 
